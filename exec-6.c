ec97060ccb1cb68ff3b9face02582e1c4200372a
hayverson <hayversongab@outlook.com>
2025-05-12 13:11:02 -0300

    Create exec-6.c

---
+++ exec-6.c
@@ -0,0 +1,10 @@
+/*
+Escreva um programa solicite ao usuário uma sequência de caracteres
+sem limite de tamanho máximo e realize as seguintes operações usando
+uma pilha:
+a) Imprimir o texto na ordem inversa;
+b) Verificar se o texto é um palíndromo, ou seja, se o texto é escrito da
+mesma maneira de frente para trás e de trás para frente. Ignore espaços
+e pontos.
+*/
+
